5f2f60b8245a1488f9aad555a3d9435553600f64
Akil Griffiths <akilgg@gmail.com>
2020-04-22 15:24:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,6 +13,14 @@ def using_pop(array)
 end
 
 def pop_with_args(array)
-puts array.pop()
-puts array.pop()
+ array.pop(2)
+
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
 end
